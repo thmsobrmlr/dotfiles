9c164af0eb31f8c49523ee470e65e6ced15a6cb3
Thomas Obermüller <thomas.obermueller@gmail.com>
2026-05-05 14:52:34 +0200

    keybindings

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,9 @@
-ln -sfv $PWD/vim $HOME/.vim;
+#!/bin/sh
+
+ln -sfv "$PWD/vim" "$HOME/.vim";
 
 cd symlink
-for f in *; do ln -sfv $PWD/$f $HOME/.$f; done
+for f in *; do ln -sfv "$PWD/$f" "$HOME/.$f"; done
+
+mkdir -p "$HOME/Library/Application Support/Code/User"
+ln -sfv "$PWD/../vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
